db030402dfea8b082cd9e255fb2d50fd2358a77d
GodFather93 <suryapalsingh.inda@gmail.com>
2019-11-08 09:45:13 +0530

    UpdatedWithNewAddress and Func to send coins

--- src/Screens/WalletNewAddressScreen/createAnAddress.js
+++ src/Screens/WalletNewAddressScreen/createAnAddress.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default createAnAddress = async() =>{
  
-   return await axios.get(`https://block.io/api/v2/get_new_address/?api_key=e1d0-4d39-2e4f-6bb6`)
+   return await axios.get(`https://block.io/api/v2/get_new_address/?api_key=69ac-eaa6-b094-5f0d`)
 
 }
